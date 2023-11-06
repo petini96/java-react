b59ea2b7c2a7289f1d0a226d5f2c35454d3f10d1
Vinícius <petiniprojetos96@gmail.com>
2023-11-06 15:53:45 -0400

    alterações finais

--- src/main/java/com/example/javareact/controller/HelloController.java
+++ src/main/java/com/example/javareact/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.javareact.controller;
  
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/example/javareact/controller/TokenController.java
+++ src/main/java/com/example/javareact/controller/TokenController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
